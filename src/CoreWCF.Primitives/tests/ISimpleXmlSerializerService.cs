10b4491fca6b90bfb98caecc454b02c01e4b0510
Jeroen-bart Engelen <jeroenbart.engelen@kpn.com>
2019-09-22 17:01:22 +0200

    Made XmlSerializerFormatAttribute and releated enums public and available for use, like in the full .NET Framework.

---
+++ src/CoreWCF.Primitives/tests/ISimpleXmlSerializerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreWCF.Primitives.Tests
+{
+    [XmlSerializerFormat]
+    [ServiceContract]
+    interface ISimpleXmlSerializerService
+    {
+        [OperationContract]
+        string Echo(string echo);
+    }
+}
