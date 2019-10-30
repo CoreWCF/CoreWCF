11228e64a6f351daefd7f21ead6cd9a2d0f5fd92
Matt Connew <matt.connew+github@outlook.com>
2019-10-30 13:50:19 -0700

    Fix connection reuse problems
    Streamed transfer mode for NetTcp wasn't using our own RawStream
    implementation which can safely and cleanly unwrap an upgraded stream.
    This was preventing the socket from being reused for multiple requests
    as NegotiateStream still had a pending read on the raw connection.
    Fixed an issue when the socket is closed while trying to read the
    framing mode.
    Fixed tests to correctly close the channel factory which results in
    pooled connections being correctly closed. Added a helper to ensure the
    channel and factory are fully closed even if there's an exception.
    Added logger when building tests in Debug mode which outputs AspNetCore
    logs to ITestOutputHelper (xunit class) for capturing any error logging
    from AspNetCore.
    Added binding helper to configure the client bindings to have a 20
    minute timeout for all timeouts when a debugger is attached. This is to
    help when stepping through code while debugging a test without having
    the connection aborted due to a timeout.

---
+++ src/CoreWCF.NetTcp/tests/Helpers/ServiceHelper.cs
@@ -0,0 +1,58 @@
+﻿using CoreWCF.Configuration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using Xunit.Abstractions;
+
+namespace Helpers
+{
+    public static class ServiceHelper
+    {
+        public static IWebHostBuilder CreateWebHostBuilder<TStartup>(ITestOutputHelper outputHelper) where TStartup : class =>
+            WebHost.CreateDefaultBuilder(new string[0])
+#if DEBUG
+            .ConfigureLogging((ILoggingBuilder logging) =>
+            {
+                logging.AddProvider(new XunitLoggerProvider(outputHelper));
+                logging.AddFilter("Default", LogLevel.Debug);
+                logging.AddFilter("Microsoft", LogLevel.Debug);
+                logging.SetMinimumLevel(LogLevel.Debug);
+            })
+#endif // DEBUG
+            .UseNetTcp(8808)
+            .UseStartup<TStartup>();
+
+        public static void CloseServiceModelObjects(params System.ServiceModel.ICommunicationObject[] objects)
+        {
+            foreach (System.ServiceModel.ICommunicationObject comObj in objects)
+            {
+                try
+                {
+                    if (comObj == null)
+                    {
+                        continue;
+                    }
+                    // Only want to call Close if it is in the Opened state
+                    if (comObj.State == System.ServiceModel.CommunicationState.Opened)
+                    {
+                        comObj.Close();
+                    }
+                    // Anything not closed by this point should be aborted
+                    if (comObj.State != System.ServiceModel.CommunicationState.Closed)
+                    {
+                        comObj.Abort();
+                    }
+                }
+                catch (TimeoutException)
+                {
+                    comObj.Abort();
+                }
+                catch (System.ServiceModel.CommunicationException)
+                {
+                    comObj.Abort();
+                }
+            }
+        }
+    }
+}
