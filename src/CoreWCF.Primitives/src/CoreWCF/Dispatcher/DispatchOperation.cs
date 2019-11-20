8e7509af9a5da2179e73d57089d32978c2e01724
Mike Treit <mtreit@microsoft.com>
2019-11-20 12:39:36 -0800

    Make the ParameterInspectors collection on DispatchOperation public (#72)
    * Make the ParameterInspectors collection on DispatchOperation public, to allow WCF services to register their own parameter inspectors.
    * Switch versioning in project file to use VersionPrefix and VersionSuffix

--- src/CoreWCF.Primitives/src/CoreWCF/Dispatcher/DispatchOperation.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/Dispatcher/DispatchOperation.cs
@@ -188,7 +188,7 @@ namespace CoreWCF.Dispatcher
             get { return name; }
         }
 
-        internal SynchronizedCollection<IParameterInspector> ParameterInspectors { get; }
+        public ICollection<IParameterInspector> ParameterInspectors { get; }
 
         public DispatchRuntime Parent { get; }
 
