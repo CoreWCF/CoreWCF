506c46f14955de1d753cfb9951d64a454bd20896
Carol Wang <v-carwan@microsoft.com>
2020-06-17 12:47:43 -0700

    Negative test - operation contract null Action and null ReplyAction throws. (#137)

---
+++ src/CoreWCF.Http/tests/ServiceContract/IOpContractInvalidActionReplyAction.cs
@@ -0,0 +1,18 @@
+﻿using CoreWCF;
+
+namespace ServiceContract
+{
+    [ServiceContract]
+    interface IOpContractInvalidAction
+    {
+        [OperationContract(Action = null)]
+        void TestMethodNullAction(int id);
+    }
+
+    [ServiceContract]
+    interface IOpContractInvalidReplyAction
+    {
+        [OperationContract(ReplyAction = null)]
+        int TestMethodNullReplyAction(int id);
+    }
+}
