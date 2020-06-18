33b29c549287e30f7168bc5d7297cab3af82c298
Carol Wang <v-carwan@microsoft.com>
2020-06-18 02:46:27 -0700

    ActionReplyAction behavior test.

---
+++ src/CoreWCF.Http/tests/ServiceContract/IOpActionReplyActionBehavior.cs
@@ -0,0 +1,27 @@
+﻿using CoreWCF;
+using CoreWCF.Channels;
+
+namespace ServiceContract
+{
+    [ServiceContract(Name = "IOpActionReplyActionBehavior")]
+    public interface IOpActionReplyActionBehavior
+    {
+        [OperationContract]
+        int TestMethodCheckDefaultReplyAction(int ID, string name);
+
+        [OperationContract(ReplyAction = "myReplyAction")]
+        int TestMethodCheckCustomReplyAction(int ID, string name);
+
+        [OperationContract(ReplyAction = "http://myReplyAction")]
+        int TestMethodCheckUriReplyAction(int ID, string name);
+
+        [OperationContract(ReplyAction = "")]
+        int TestMethodCheckEmptyReplyAction(int ID, string name);
+
+        [OperationContract(ReplyAction = "*")]
+        Message TestMethodCheckUntypedReplyAction();
+
+        [OperationContract(IsOneWay = true, Action = "*")]
+        void UnMatchedMessageHandler(Message m);
+    }
+}
