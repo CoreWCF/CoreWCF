704dde36d02c57264659f92a02a350916572b457
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2020-06-21 23:45:40 -0700

    FaultContracts - Send fault responses using FaultContracts - Vary the Name.

---
+++ src/CoreWCF.Http/tests/ServiceContract/ITestFaultContractName1.cs
@@ -0,0 +1,12 @@
+﻿using CoreWCF;
+
+namespace ServiceContract
+{
+    [ServiceContract]
+    public interface ITestFaultContractName1
+    {
+        [OperationContract]
+        [FaultContract(typeof(string), Name = "foo")]
+        string Method1(string s);
+    }
+}
---
+++ src/CoreWCF.Http/tests/Services/TestFaultContractName1Service.cs
@@ -0,0 +1,25 @@
+﻿using CoreWCF;
+using ServiceContract;
+
+namespace Services
+{
+    [ServiceBehavior]
+    public class FaultContract_859456_Service1 : ITestFaultContractName1
+    {
+        #region TwoWay_Methods
+        public string Method1(string s)
+        {
+            if (s.Length == 0)
+            {
+                string faultToThrow = "Test fault thrown from a service";
+                throw new FaultException<string>(faultToThrow);
+            }
+            else
+            {
+                System.Console.WriteLine("Error, Received Input: {0}", s);
+            }
+            return s;
+        }
+        #endregion
+    }
+}
