023ead3804537e52dde60a3cff1a339ec07cd1dd
v-zhyou <v-zhyou@VREMOTE>
2020-07-02 02:19:12 -0700

    Add 2 testcases in BufferManagerTests:
    case1: VerifyBufferUsed( )
    case2: VerifyBufferPools( )

---
+++ src/CoreWCF.Primitives/tests/Helpers/BufferManagerTestsCommon.cs
@@ -0,0 +1,43 @@
+﻿using CoreWCF.Channels;
+using System;
+using System.Reflection;
+
+namespace CoreWCF.Primitives.Tests.Helpers
+{
+    public class BufferManagerTestsCommon
+    {
+        private static string SynchronizedBufferPool = "synchronizedbufferpool";
+
+        private static string LargeBufferPool = "largebufferpool";
+
+        public static int TrainingCount = 64;
+
+        public static int LargeBufferLimit = 85000;
+
+        public static bool VerifyBufferPoolsCreated(BufferManager bufferManager)
+        {
+            bool flag = true;
+            PropertyInfo property = bufferManager.GetType().GetProperty("InternalBufferManager");
+            object value = property.GetValue(bufferManager, null);
+            object obj = value.GetType().InvokeMember("bufferPools", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField, null, value, null);
+            Array array = obj as Array;
+            for (int i = 0; i < array.Length; i++)
+            {
+                object value2 = array.GetValue(i);
+                PropertyInfo property2 = value2.GetType().GetProperty("BufferSize");
+                int num = (int)property2.GetValue(value2, null);
+                string text = value2.GetType().ToString();
+                if (num < LargeBufferLimit)
+                {
+                    flag &= text.ToLower().Contains(SynchronizedBufferPool);
+                }
+                else
+                {
+                    flag &= text.ToLower().Contains(LargeBufferPool);
+                }
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ src/CoreWCF.Primitives/tests/VerifyBufferPools.cs
@@ -0,0 +1,36 @@
+﻿using CoreWCF.Channels;
+using CoreWCF.Primitives.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreWCF.Primitives.Tests
+{
+    public class VerifyBufferPoolsCreated
+    {
+        private bool testPassed = true;
+        private ITestOutputHelper _output;
+
+        public VerifyBufferPoolsCreated(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void VerifyBufferPoolsCreatedtest()
+        {
+            for (int i = 80000; i <= 90000; i += 5000)
+            {
+                BufferManager bufferManager = BufferManager.CreateBufferManager(524288L, i);
+                bool flag = BufferManagerTestsCommon.VerifyBufferPoolsCreated(bufferManager);
+                testPassed &= flag;
+                _output.WriteLine("Correct BufferPools are created for maxbuffersize:{0} {1} ", new object[]
+                {
+                    i,
+                    flag
+                });
+
+                Assert.True(testPassed);
+            }
+        }
+    }
+}
