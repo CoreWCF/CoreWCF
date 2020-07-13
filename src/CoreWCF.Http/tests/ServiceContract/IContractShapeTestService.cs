5264a511d0de6659d1a65c684d0f21d323b5b776
Carol Wang <v-carwan@microsoft.com>
2020-07-13 01:14:06 -0700

    Contract shape test.

---
+++ src/CoreWCF.Http/tests/ServiceContract/IContractShapeTestService.cs
@@ -0,0 +1,38 @@
+﻿using CoreWCF;
+using System.Runtime.Serialization;
+
+namespace ServiceContract
+{
+    [DataContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public class SM_ComplexType
+    {
+        [DataMember]
+        public int n;
+        [DataMember]
+        public string s;
+    }
+
+    [ServiceContract(Name = "IContractService")]
+    interface IServiceContract_Overloads
+    {
+        [OperationContract(Name = "TwoWayInt")]
+        string TwoWayMethod(int n);
+
+        [OperationContract(Name = "TwoWayString")]
+        string TwoWayMethod(string s);
+
+        [OperationContract(Name = "TwoWayComplex")]
+        string TwoWayMethod(SM_ComplexType ct);
+
+        [OperationContract(Name = "TwoWayVoid")]
+        string TwoWayMethod();
+    }
+
+    [ServiceContract(Name = "IContractService")]
+    interface IServiceContract_Params
+    {
+        // Two Way w/ Parameter Array
+        [OperationContract]
+        string TwoWayParamArray(int n, params int[] args);
+    }
+}
---
+++ src/CoreWCF.Http/tests/Services/ContractShapeTestServices.cs
@@ -0,0 +1,47 @@
+﻿using CoreWCF;
+using ServiceContract;
+using Xunit;
+
+namespace Services
+{
+    [ServiceBehavior]
+    public class ContractShapeOverloadsService : IServiceContract_Overloads
+    {
+        public string TwoWayMethod()
+        {
+            return string.Format("Server Received: Void");
+        }
+
+        public string TwoWayMethod(int n)
+        {
+            return string.Format("Server Received: {0}", n);
+        }
+
+        public string TwoWayMethod(string s)
+        {
+            return string.Format("Server Received: {0}", s);
+        }
+
+        public string TwoWayMethod(SM_ComplexType ct)
+        {
+            return string.Format("Server Received: {0} and {1}", ct.n, ct.s);
+        }
+    }
+
+    [ServiceBehavior]
+    public class ContractShapeParamsService : IServiceContract_Params
+    {
+        public string TwoWayParamArray(int n, params int[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] != i)
+                {
+                    Assert.Equal(i, args[i]);
+                }
+            }
+
+            return string.Format("Service recieved and processed {0} args", args.Length);
+        }
+    }
+}
