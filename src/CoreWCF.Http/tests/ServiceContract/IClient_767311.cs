f773aae9daedfda6f12f34b7a0220779de29795f
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2020-07-14 02:32:41 -0700

    Create different variations of service signatures and call from async client.

---
+++ src/CoreWCF.Http/tests/ServiceContract/IClient_767311.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CoreWCF;
+
+namespace ServiceContract
+{
+
+    [ServiceContract]
+    public interface ISyncService
+    {
+        [OperationContract]
+        string EchoString(string s);
+    }
+}
