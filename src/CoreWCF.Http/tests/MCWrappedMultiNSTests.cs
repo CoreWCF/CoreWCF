42aad2dd3d6c81d825957177234235225ffa5c36
SunnyChen <55071061+v-fangfc@users.noreply.github.com>
2020-08-11 08:48:37 +0800

    Multiple DataContracts in different namespaces. (#148)

---
+++ src/CoreWCF.Http/tests/MCWrappedMultiNSTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClientContract;
+using CoreWCF.Configuration;
+using Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreWCF.Http.Tests
+{
+    public class MCWrappedMultiNSTests
+    {
+        private ITestOutputHelper _output;
+
+        public MCWrappedMultiNSTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void MulDataContractsInDiffNS()
+        {
+            var host = ServiceHelper.CreateWebHostBuilder<Startup>(_output).Build();
+            using (host)
+            {
+                host.Start();
+                var httpBinding = ClientHelper.GetBufferedModeBinding();
+                var factory = new System.ServiceModel.ChannelFactory<ClientContract.IMCWrappedMultiNS>(httpBinding,
+                    new System.ServiceModel.EndpointAddress(new Uri("http://localhost:8080/BasicWcfService/MCWrappedMultiNSService.svc")));
+                var channel = factory.CreateChannel();
+
+                MC2MultiNS RetMC = channel.M(new MCMultiNS());
+                Assert.NotNull(RetMC);
+            }
+        }
+
+        internal class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddServiceModelServices();
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseServiceModel(builder =>
+                {
+                    builder.AddService<Services.ServerWrappedMultipleNSService>();
+                    builder.AddServiceEndpoint<Services.ServerWrappedMultipleNSService, ServiceContract.IMCWrappedMultiNS>(new BasicHttpBinding(), "/BasicWcfService/MCWrappedMultiNSService.svc");
+                });
+            }
+        }
+    }
+}
