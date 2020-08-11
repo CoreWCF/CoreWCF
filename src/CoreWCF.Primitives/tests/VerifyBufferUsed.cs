fa0bde82ddf0cf204c0bb254f7fb1bf9c057f7ea
william you <v-zhyou@microsoft.com>
2020-08-12 02:25:04 +0800

    Add 2 BufferManagerTests testcases (#161)
    * Add 2 testcases in BufferManagerTests:
    
    case1: VerifyBufferUsed( )
    case2: VerifyBufferPools( )
    
    * Format document
    
    Co-authored-by: v-zhyou <v-zhyou@VREMOTE>

---
+++ src/CoreWCF.Primitives/tests/VerifyBufferUsed.cs
@@ -0,0 +1,72 @@
+﻿using CoreWCF.Channels;
+using CoreWCF.Primitives.Tests.Helpers;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreWCF.Primitives.Tests
+{
+    public class VerifyBufferUsed
+    {
+        private bool testPassed = true;
+        private static byte[] buffer;
+        private ITestOutputHelper _output;
+
+        public VerifyBufferUsed(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void VerifyBufferUsedbyThreadsTest()
+        {
+            for (int i = 80000; i <= 90000; i += 5000)
+            {
+                _output.WriteLine("Training Buffer of size = " + i);
+                for (int j = 63; j <= BufferManagerTestsCommon.TrainingCount; j++)
+                {
+                    BufferPoolAffinitize(j, i);
+                }
+            }
+
+            Assert.True(testPassed);
+        }
+
+        private void BufferPoolAffinitize(int trainingCount, int bufferSize)
+        {
+            BufferManager bufferManager = BufferManager.CreateBufferManager(524288L, bufferSize);
+            buffer = null;
+            for (int i = 0; i < trainingCount; i++)
+            {
+                buffer = bufferManager.TakeBuffer(bufferSize);
+                bufferManager.ReturnBuffer(buffer);
+            }
+
+            _output.WriteLine("Same buffers returned for bufferSize={0} and  training Count={1} : ", new object[]
+            {
+                bufferSize,
+                trainingCount
+            });
+
+            Thread thread = new Thread(delegate ()
+            {
+                byte[] array = bufferManager.TakeBuffer(bufferSize);
+                bool flag = array == buffer;
+                if (bufferSize < BufferManagerTestsCommon.LargeBufferLimit && trainingCount == BufferManagerTestsCommon.TrainingCount)
+                {
+                    testPassed &= !flag;
+                }
+                else
+                {
+                    testPassed &= flag;
+                }
+
+                _output.WriteLine(flag.ToString());
+            });
+
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
+
