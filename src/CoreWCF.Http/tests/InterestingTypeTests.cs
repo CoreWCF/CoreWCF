418d8eba7515ebe911a87d5d8b7dfc80d84d1b4a
Carol Wang <v-carwan@microsoft.com>
2020-08-14 03:07:58 +0800

    Add interesting type test port from NetFramework. (#153)
    * Add interesting type test port from NetFramework.

---
+++ src/CoreWCF.Http/tests/InterestingTypeTests.cs
@@ -0,0 +1,110 @@
+﻿using ClientContract;
+using CoreWCF.Configuration;
+using Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreWCF.Http.Tests
+{
+    public class InterestingTypeTests
+    {
+        private ITestOutputHelper _output;
+
+        public InterestingTypeTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TypedContractCollectionTest()
+        {
+            var host = ServiceHelper.CreateWebHostBuilder<TypedContractCollectionServiceStartup>(_output).Build();
+            using (host)
+            {
+                host.Start();
+                var httpBinding = ClientHelper.GetBufferedModeBinding();
+                var factory = new System.ServiceModel.ChannelFactory<ITypedContract_Collection>(httpBinding,
+                    new System.ServiceModel.EndpointAddress(new Uri("http://localhost:8080/BasicWcfService/TypedContract_CollectionService.svc")));
+                var channel = factory.CreateChannel();
+                
+                foreach (int numItems in  new int[] {1, 5, 15, 50 })
+                {
+                    //arraylist
+                    var outgoingAL = new ArrayList();
+                    for (int item = 0; item < numItems; item++)
+                    {
+                        outgoingAL.Add(item);
+                    }
+
+                    ArrayList responseAL = channel.ArrayListMethod(outgoingAL);
+                    Assert.Equal(outgoingAL.Count, responseAL.Count);
+                    for (int item = 0; item < responseAL.Count; item++)
+                    {
+                        if ((int)responseAL[item] != (int)outgoingAL[item])
+                        {
+                            Assert.True(false, "ArrayList item validation failed");
+                        }
+                    }
+
+                    //Collection
+                    var outgoingCL = new Collection<string>();
+                    for (int item = 0; item < numItems; item++)
+                    {
+                        string s = string.Format("Item " + item);
+                        outgoingCL.Add(s);
+                    }
+
+                    Collection<string> responseCL = channel.CollectionOfStringsMethod(outgoingCL);
+                    Assert.Equal(outgoingCL.Count, responseCL.Count);
+                    for (int item = 0; item < responseCL.Count; item++)
+                    {
+                        if (responseCL[item].CompareTo(outgoingCL[item]) != 0)
+                        {
+                            Assert.True(false, "Collection item validation failed");
+                        }
+                    }
+
+                    //CollecitonBase
+                    MyCollection outgoingCB = new MyCollection();
+                    for (int item = 0; item < numItems; item++)
+                    {
+                        outgoingCB.Add((short)item);
+                    }
+
+                    MyCollection responseCB = channel.CollectionBaseMethod(outgoingCB);
+                    Assert.Equal(outgoingCB.Count, responseCB.Count);
+                    for (int item = 0; item < responseCB.Count; item++)
+                    {
+                        if (responseCB[item] != outgoingCB[item])
+                        {
+                            Assert.True(false, "MyCollection:CollectionBase item validation failed");
+                        }
+                    }
+                }
+            }
+        }
+
+        internal class TypedContractCollectionServiceStartup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddServiceModelServices();
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseServiceModel(builder =>
+                {
+                    builder.AddService<Services.TypedContract_CollectionService>();
+                    builder.AddServiceEndpoint<Services.TypedContract_CollectionService, ServiceContract.ITypedContract_Collection>(new BasicHttpBinding(), "/BasicWcfService/TypedContract_CollectionService.svc");
+                });
+            }
+        }
+    }
+}
---
+++ src/CoreWCF.Http/tests/Services/TypedContract_CollectionService.cs
@@ -0,0 +1,36 @@
+﻿using ServiceContract;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Services
+{
+    public class TypedContract_CollectionService : ITypedContract_Collection
+    {
+        public ArrayList ArrayListMethod(ArrayList collection)
+        {
+            return (ArrayList)collection.Clone();
+        }
+
+        public Collection<string> CollectionOfStringsMethod(Collection<string> collection)
+        {
+            Collection<string> returnList = new Collection<string>();
+            foreach (string s in collection)
+            {
+                returnList.Add(s);
+            }
+
+            return returnList;
+        }
+
+        public MyCollection CollectionBaseMethod(MyCollection collection)
+        {
+            MyCollection returnList = new MyCollection();
+            foreach (short i in collection)
+            {
+                returnList.Add(i);
+            }
+
+            return returnList;
+        }
+    }
+}
