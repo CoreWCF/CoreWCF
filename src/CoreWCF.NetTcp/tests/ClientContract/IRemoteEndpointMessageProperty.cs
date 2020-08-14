ce54af4ed349e47ea9b6d12e5df8323d91f9dfc2
Carol Wang <v-carwan@microsoft.com>
2020-08-14 08:40:03 +0800

    Test the RemoteEndpointMessageProperty. (#188)

---
+++ src/CoreWCF.NetTcp/tests/ClientContract/IRemoteEndpointMessageProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace ClientContract
+{
+    [ServiceContract]
+    public interface IRemoteEndpointMessageProperty
+    {
+        [OperationContract(Action = "*", ReplyAction = "*")]
+        Message Echo(Message input);
+    }
+}
---
+++ src/CoreWCF.NetTcp/tests/Services/RemoteEndpointMessagePropertyService.cs
@@ -0,0 +1,14 @@
+﻿using CoreWCF.Channels;
+using ServiceContract;
+
+namespace Services
+{
+    public class RemoteEndpointMessagePropertyService : IRemoteEndpointMessageProperty
+    {
+        public Message Echo(Message input)
+        {
+            RemoteEndpointMessageProperty remp = (RemoteEndpointMessageProperty)input.Properties[RemoteEndpointMessageProperty.Name];
+            return Message.CreateMessage(input.Version, "echo", input.GetBody<string>()+";"+ remp.Address+";"+ remp.Port.ToString());
+        }
+    }
+}
