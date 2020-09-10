5665a736f95d7f292cd13856844c3676b780b152
Daniel <daniel.crenna@gmail.com>
2020-09-10 14:48:10 -0400

    nit: IServiceBuilder chaining (#212)
    * nit: IServiceBuilder chaining
    * fix failing test
    * remove unnecessary use of InternalsVisibleTo

--- src/CoreWCF.Primitives/src/CoreWCF/Configuration/IServiceBuilder.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/Configuration/IServiceBuilder.cs
@@ -9,16 +9,17 @@ namespace CoreWCF.Configuration
     {
         ICollection<Type> Services { get; }
         ICollection<Uri> BaseAddresses { get; }
-        void AddService<TService>() where TService : class;
-        void AddService(Type service);
-        void AddServiceEndpoint<TService, TContract>(Binding binding, string address);
-        void AddServiceEndpoint<TService, TContract>(Binding binding, Uri address);
-        void AddServiceEndpoint<TService, TContract>(Binding binding, string address, Uri listenUri);
-        void AddServiceEndpoint<TService, TContract>(Binding binding, Uri address, Uri listenUri);
-        void AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address);
-        void AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address);
-        void AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address, Uri listenUri);
-        void AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address, Uri listenUri);
-        void AddServiceEndpoint(Type service, Type implementedContract, Binding binding, Uri address, Uri listenUri);
+
+        IServiceBuilder AddService<TService>() where TService : class;
+        IServiceBuilder AddService(Type service);
+        IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address);
+        IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address);
+        IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address, Uri listenUri);
+        IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address, Uri listenUri);
+        IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address);
+        IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address);
+        IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address, Uri listenUri);
+        IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address, Uri listenUri);
+        IServiceBuilder AddServiceEndpoint(Type service, Type implementedContract, Binding binding, Uri address, Uri listenUri);
     }
 }
---
+++ src/CoreWCF.Primitives/tests/ServiceBuilderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CoreWCF.Channels;
+using CoreWCF.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CoreWCF.Primitives.Tests
+{
+    public class ServiceBuilderTests
+    {
+        [Fact]
+        public void ServiceBuilderCanChain()
+        {
+            var services = new ServiceCollection();
+            services.AddServiceModelServices();
+            var serviceProvider = services.BuildServiceProvider();
+            var builder = serviceProvider.GetRequiredService<IServiceBuilder>();
+            
+            Assert.Equal(builder, builder.AddService<SomeService>());
+            Assert.Equal(builder, builder.AddService(typeof(SomeService)));
+            Assert.Equal(builder, builder.AddServiceEndpoint<SomeService, IService>(new NoBinding(), new Uri("http://localhost:8088/SomeService.svc")));
+            Assert.Equal(builder, builder.AddServiceEndpoint<SomeService, IService>(new NoBinding(), "http://localhost:8088/SomeService.svc"));
+            Assert.Equal(builder, builder.AddServiceEndpoint<SomeService>(typeof(IService), new NoBinding(), new Uri("http://localhost:8088/SomeService.svc")));
+            Assert.Equal(builder, builder.AddServiceEndpoint<SomeService>(typeof(IService), new NoBinding(), "http://localhost:8088/SomeService.svc"));
+        }
+
+        public class SomeService : IService { }
+
+        public class NoBinding : Binding
+        {
+            public override string Scheme => "none";
+            public override BindingElementCollection CreateBindingElements()
+            {
+                return new BindingElementCollection();
+            }
+        }
+    }
+}
