5d484d890784002d53874d6f1168a5acff67f77b
Biroj Nayak <49173255+birojnayak@users.noreply.github.com>
2020-10-19 20:43:03 +0000

    Fix #238 (#240)
    Fixes #238

---
+++ src/CoreWCF.Http/tests/ClientContract/IServiceWithMessageBodyAndHeader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClientContract
+{
+    [ServiceContract]
+    public interface IServiceWithMessageBodyAndHeader
+    {
+        [OperationContract]
+        string Echo(string echo);
+
+        [OperationContract]
+        CoreEchoMessageResponse EchoWithMessageContract(CoreEchoMessageRequest request);
+    }
+
+    [MessageContract]
+    public class CoreEchoMessageRequest
+    {
+        [MessageBodyMember]
+        public string Text { get; set; }
+
+        [MessageHeader]
+        public string APIKey { get; set; }
+    }
+
+
+    [MessageContract]
+    public class CoreEchoMessageResponse
+    {
+        [MessageBodyMember]
+        public string SayHello { get; set; }
+
+        [MessageHeader]
+        public string SayHi { get; set; }
+    }
+}
---
+++ src/CoreWCF.Http/tests/ServiceWithMessageBodyAndHeaderTest.cs
@@ -0,0 +1,61 @@
+﻿using CoreWCF.Configuration;
+using ClientContract;
+using Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using CoreWCF;
+
+namespace BasicHttp
+{
+   public class ServiceWithMessageBodyAndHeaderTest
+    {
+        private ITestOutputHelper _output;
+
+        public ServiceWithMessageBodyAndHeaderTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void BasicScenarioServiceMessageBody()
+        {
+            var host = ServiceHelper.CreateWebHostBuilder<Startup>(_output).Build();
+            using (host)
+            {
+                host.Start();
+                var httpBinding = ClientHelper.GetBufferedModeBinding();
+                var factory = new System.ServiceModel.ChannelFactory<ClientContract.IServiceWithMessageBodyAndHeader>(httpBinding,
+                    new System.ServiceModel.EndpointAddress(new Uri("http://localhost:8080/BasicWcfService/Service.svc")));
+                var channel = factory.CreateChannel();
+
+             
+                CoreEchoMessageResponse result = channel.EchoWithMessageContract (new CoreEchoMessageRequest() { Text = "Message Hello", APIKey = "DEVKEYTOTEST" });
+                Assert.NotNull(result);
+
+            }
+        }
+
+        internal class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddServiceModelServices();
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseServiceModel(builder =>
+                {
+                    builder.AddService<Services.ServiceWithMessageBodyAndHeader>();
+                    builder.AddServiceEndpoint<Services.ServiceWithMessageBodyAndHeader, Services.IServiceWithMessageBodyAndHeader>(new BasicHttpBinding(), "/BasicWcfService/Service.svc");
+                });
+            }
+        }
+    }
+}
