8de7bfea924076dc8e0166e0a0ca6dabb2187fe5
v-joesun@microsoft.com <v-joesun@microsoft.com>
2020-12-17 16:45:14 +0800

    Add CoreWCFPerf tests

---
+++ src/Crank/CoreWCFPerfService/SayHello.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace CoreWCFPerfService
+{
+    public class SayHello : ISayHello
+    {
+        public Task<string> HelloAsync(string name)
+        {
+            return Task.Factory.StartNew(() => { return string.Format("Hello {0}", name); });
+        }
+    }
+}
