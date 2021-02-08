8329aa2873aac81c5feee0c8630391e81e0352dd
v-joesun@microsoft.com <v-joesun@microsoft.com>
2021-02-08 18:03:37 +0800

    Modify the hostbuilder

--- src/Crank/CoreWCFPerfService/Program.cs
+++ src/Crank/CoreWCFPerfService/Program.cs
@@ -18,8 +18,6 @@ namespace CoreWCFPerfService
 
     public class Program
     {
-        private string _paramUrl;
-
         static void Main(string[] args)
         {
             string filePath = Path.Combine(Environment.CurrentDirectory, "CoreWCFPerfService.exe");
@@ -28,12 +26,8 @@ namespace CoreWCFPerfService
             ExecuteCommand(command, Environment.CurrentDirectory, TimeSpan.FromSeconds(20));
             Console.WriteLine("Application start.");
 
-            Program test = new Program();
-            if (test.ProcessRunOptions(args))
+            using (var host = CreateWebHostBuilder(args).Build())
             {
-                Uri baseAddress = new Uri(test._paramUrl);
-                
-                var host = CreateWebHostBuilder<Startup>(test._paramUrl,baseAddress.Scheme,baseAddress.Port).Build();
                 host.Start();
 
                 Console.WriteLine("Service is Ready");
@@ -46,20 +40,11 @@ namespace CoreWCFPerfService
             Console.WriteLine("Clean up the firewall rule.");            
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder<TStartup>(string url,string protocol,int port) where TStartup : class
-        {
-            var hostBuilder = WebHost.CreateDefaultBuilder(new string[0]);
-            if (string.Equals(protocol, "http", StringComparison.InvariantCultureIgnoreCase))
-            {
-                hostBuilder = hostBuilder.UseUrls(url);
-            }
-            else
-            {
-                hostBuilder = hostBuilder.UseNetTcp(port);
-            }
-            
-            return hostBuilder.UseStartup<TStartup>();
-        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => { options.ListenLocalhost(8080); })
+            .UseNetTcp(8808)
+            .UseStartup<Startup>();
 
         public class Startup
         {
@@ -75,33 +60,8 @@ namespace CoreWCFPerfService
                     builder.AddService<SayHello>();
                     builder.AddServiceEndpoint<SayHello, ISayHello>(new BasicHttpBinding(), "/SayHello/SayHello.svc");
                     builder.AddServiceEndpoint<SayHello, ISayHello>(new NetTcpBinding(SecurityMode.None), "/SayHello/SayHello.svc");
-
                 });
             }
-        }
-
-        private bool ProcessRunOptions(string[] args)
-        {
-            foreach (string s in args)
-            {
-                string[] p = s.Split(new char[] { ':' }, count: 2);
-                if (p.Length != 2)
-                {
-                    continue;
-                }
-
-                switch (p[0].ToLower())
-                {
-                    case Parameters.Url:
-                        _paramUrl = p[1];
-                        break;
-
-                    default:
-                        Console.WriteLine("unknown argument: " + s);
-                        continue;
-                }
-            }
-            return true;
         }
 
         private static int ExecuteCommand(string command, string workingDirectory, TimeSpan timeout)
@@ -109,12 +69,15 @@ namespace CoreWCFPerfService
             Process process = new Process();
             process.StartInfo.FileName = "netsh";
             process.StartInfo.Arguments = command;
+
             if (workingDirectory != null)
             {
                 process.StartInfo.WorkingDirectory = workingDirectory;
             }
+
             process.StartInfo.UseShellExecute = false;
             process.Start();
+
             bool flag;
             if (timeout.TotalMilliseconds >= Int32.MaxValue)
             {
@@ -124,6 +87,7 @@ namespace CoreWCFPerfService
             {
                 flag = process.WaitForExit((int)timeout.TotalMilliseconds);
             }
+
             if (!flag)
             {
                 process.Kill();
@@ -137,6 +101,7 @@ namespace CoreWCFPerfService
                     timeout.ToString()
                 }));
             }
+
             return process.ExitCode;
         }
     }
