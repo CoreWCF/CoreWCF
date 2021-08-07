63fa7c10605451d14e3eec64e3bad6d12bd60fa4
g7ed6e <guillaume.delahaye@outlook.com>
2021-08-07 12:30:06 +0200

    make DispatchOperation.FaultContractInfos public

---
+++ src/CoreWCF.Http/tests/ClientContract/ICalculatorService.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Serialization;
+
+namespace ClientContract
+{
+    [System.ServiceModel.ServiceContract]
+    public interface ICalculatorService
+    {
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.FaultContract(typeof(MathFault))]
+
+        int Divide(int a, int b);
+    }
+
+    [DataContract]
+    public class MathFault
+    {
+        [DataMember]
+        public string Error { get; set; }
+    }
+}
