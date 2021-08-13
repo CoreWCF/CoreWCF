623358927ddb15c0d7ec6ded8d9bc15818e98342
Petar Petrov <ppetrov@david.bg>
2021-08-13 10:24:30 +0300

    Add more complex SOAP type tests

--- src/CoreWCF.Primitives/tests/Soap/EchoSoapService.cs
+++ src/CoreWCF.Primitives/tests/Soap/EchoSoapService.cs
@@ -3,14 +3,54 @@
 
 namespace CoreWCF.Primitives.Tests.Soap
 {
+    [System.Serializable]
+    [System.Xml.Serialization.SoapType]
+    public class InnerComplexMessage : System.IEquatable<InnerComplexMessage>
+    {
+        private System.Guid _guid;
+
+        public System.Guid Guid
+        {
+            get => _guid;
+            set => _guid = value;
+        }
+
+        public bool Equals(InnerComplexMessage other)
+            => other != null
+                && _guid == other._guid;
+    }
+
+    [System.Serializable]
+    [System.Xml.Serialization.SoapType]
+    public class ComplexMessage : System.IEquatable<ComplexMessage>
+    {
+        private InnerComplexMessage[] _entitiesField;
+        private System.DateTime _date;
+
+        /// <remarks/>
+        public InnerComplexMessage[] Entities
+        {
+            get => _entitiesField;
+            set => _entitiesField = value;
+        }
+
+        /// <remarks/>
+        public System.DateTime Date
+        {
+            get => _date;
+            set => _date = value;
+        }
+
+        public bool Equals(ComplexMessage other)
+            => other != null
+                && _date == other._date
+                && _entitiesField?.Length == other._entitiesField?.Length
+                && System.Linq.Enumerable.SequenceEqual(_entitiesField, other._entitiesField);
+    }
+
     [System.ServiceModel.ServiceContract]
     public interface IEchoSoapService
     {
-        [System.ServiceModel.OperationContract]
-        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc)]
-        [return: System.ServiceModel.MessageParameter(Name = "return")]
-        string Echo(string echo);
-
         [System.ServiceModel.OperationContract]
         [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Encoded)]
         [return: System.ServiceModel.MessageParameter(Name = "return")]
@@ -20,17 +60,34 @@ namespace CoreWCF.Primitives.Tests.Soap
         [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Literal)]
         [return: System.ServiceModel.MessageParameter(Name = "return")]
         string EchoLiteral(string echo);
+
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Encoded)]
+        [System.ServiceModel.ServiceKnownType(typeof(ComplexMessage))]
+        [System.ServiceModel.ServiceKnownType(typeof(InnerComplexMessage))]
+        [return: System.ServiceModel.MessageParameter(Name = "return")]
+        ComplexMessage GetComplexMessageEncoded([System.ServiceModel.MessageParameter(Name = "message")] ComplexMessage message);
+
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Literal)]
+        [System.ServiceModel.ServiceKnownType(typeof(ComplexMessage))]
+        [System.ServiceModel.ServiceKnownType(typeof(InnerComplexMessage))]
+        [return: System.ServiceModel.MessageParameter(Name = "return")]
+        ComplexMessage GetComplexMessageLiteral([System.ServiceModel.MessageParameter(Name = "message")] ComplexMessage message);
     }
 
     public class EchoSoapService : IEchoSoapService
     {
-        public string Echo(string echo)
-            => echo;
-
         public string EchoEncoded(string echo)
             => echo;
 
         public string EchoLiteral(string echo)
            => echo;
+
+        public ComplexMessage GetComplexMessageEncoded(ComplexMessage message)
+            => message;
+
+        public ComplexMessage GetComplexMessageLiteral(ComplexMessage message)
+            => message;
     }
 }
--- src/CoreWCF.Primitives/tests/Soap/ServiceModelSoapTests.cs
+++ src/CoreWCF.Primitives/tests/Soap/ServiceModelSoapTests.cs
@@ -11,21 +11,7 @@ namespace Soap
     public class ServiceModelSoapTests
     {
         [Fact]
-        public static void OperationFormatUseNone()
-        {
-            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
-            factory.Open();
-            IEchoSoapService channel = factory.CreateChannel();
-            ((System.ServiceModel.Channels.IChannel)channel).Open();
-            string echo = channel.Echo("hello");
-            Assert.Equal("hello", echo);
-            ((System.ServiceModel.Channels.IChannel)channel).Close();
-            factory.Close();
-            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
-        }
-
-        [Fact]
-        public static void OperationFormatUseEncoded()
+        public static void Echo_OperationFormatUseEncoded()
         {
             System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
             factory.Open();
@@ -39,7 +25,7 @@ namespace Soap
         }
 
         [Fact]
-        public static void OperationFormatUseLiteral()
+        public static void Echo_OperationFormatUseLiteral()
         {
             System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
             factory.Open();
@@ -47,6 +33,56 @@ namespace Soap
             ((System.ServiceModel.Channels.IChannel)channel).Open();
             string echo = channel.EchoLiteral("hello");
             Assert.Equal("hello", echo);
+            ((System.ServiceModel.Channels.IChannel)channel).Close();
+            factory.Close();
+            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
+        }
+
+        [Fact]
+        public static void ComplexMessage_OperationFormatUseEncoded()
+        {
+            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
+            factory.Open();
+            IEchoSoapService channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)channel).Open();
+            var expected = new ComplexMessage
+            {
+                Date = System.DateTime.Now,
+                Entities = new InnerComplexMessage[]
+                {
+                    new InnerComplexMessage
+                    {
+                        Guid = System.Guid.NewGuid(),
+                    },
+                },
+            };
+            ComplexMessage actual = channel.GetComplexMessageEncoded(expected);
+            Assert.Equal(expected, actual);
+            ((System.ServiceModel.Channels.IChannel)channel).Close();
+            factory.Close();
+            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
+        }
+
+        [Fact]
+        public static void ComplexMessage_OperationFormatUseLiteral()
+        {
+            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
+            factory.Open();
+            IEchoSoapService channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)channel).Open();
+            var expected = new ComplexMessage
+            {
+                Date = System.DateTime.Now,
+                Entities = new InnerComplexMessage[]
+                {
+                    new InnerComplexMessage
+                    {
+                        Guid = System.Guid.NewGuid(),
+                    },
+                },
+            };
+            ComplexMessage actual = channel.GetComplexMessageLiteral(expected);
+            Assert.Equal(expected, actual);
             ((System.ServiceModel.Channels.IChannel)channel).Close();
             factory.Close();
             TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
