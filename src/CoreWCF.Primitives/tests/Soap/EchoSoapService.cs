a6f520be8fc61f7a83c743d5ceb55f5cf9dca529
Petar Petrov <ppetrov@david.bg>
2021-08-13 09:35:35 +0300

    Add SOAP encoding tests

---
+++ src/CoreWCF.Primitives/tests/Soap/EchoSoapService.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace CoreWCF.Primitives.Tests.Soap
+{
+    [System.ServiceModel.ServiceContract]
+    public interface IEchoSoapService
+    {
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc)]
+        [return: System.ServiceModel.MessageParameter(Name = "return")]
+        string Echo(string echo);
+
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Encoded)]
+        [return: System.ServiceModel.MessageParameter(Name = "return")]
+        string EchoEncoded(string echo);
+
+        [System.ServiceModel.OperationContract]
+        [System.ServiceModel.XmlSerializerFormat(Style = System.ServiceModel.OperationFormatStyle.Rpc, Use = System.ServiceModel.OperationFormatUse.Literal)]
+        [return: System.ServiceModel.MessageParameter(Name = "return")]
+        string EchoLiteral(string echo);
+    }
+
+    public class EchoSoapService : IEchoSoapService
+    {
+        public string Echo(string echo)
+            => echo;
+
+        public string EchoEncoded(string echo)
+            => echo;
+
+        public string EchoLiteral(string echo)
+           => echo;
+    }
+}
---
+++ src/CoreWCF.Primitives/tests/Soap/ServiceModelSoapTests.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CoreWCF.Primitives.Tests.Soap;
+using DispatcherClient;
+using Helpers;
+using Xunit;
+
+namespace Soap
+{
+    public class ServiceModelSoapTests
+    {
+        [Fact]
+        public static void OperationFormatUseNone()
+        {
+            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
+            factory.Open();
+            IEchoSoapService channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)channel).Open();
+            string echo = channel.Echo("hello");
+            Assert.Equal("hello", echo);
+            ((System.ServiceModel.Channels.IChannel)channel).Close();
+            factory.Close();
+            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
+        }
+
+        [Fact]
+        public static void OperationFormatUseEncoded()
+        {
+            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
+            factory.Open();
+            IEchoSoapService channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)channel).Open();
+            string echo = channel.EchoEncoded("hello");
+            Assert.Equal("hello", echo);
+            ((System.ServiceModel.Channels.IChannel)channel).Close();
+            factory.Close();
+            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
+        }
+
+        [Fact]
+        public static void OperationFormatUseLiteral()
+        {
+            System.ServiceModel.ChannelFactory<IEchoSoapService> factory = DispatcherHelper.CreateChannelFactory<EchoSoapService, IEchoSoapService>();
+            factory.Open();
+            IEchoSoapService channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)channel).Open();
+            string echo = channel.EchoLiteral("hello");
+            Assert.Equal("hello", echo);
+            ((System.ServiceModel.Channels.IChannel)channel).Close();
+            factory.Close();
+            TestHelper.CloseServiceModelObjects((System.ServiceModel.Channels.IChannel)channel, factory);
+        }
+    }
+}
