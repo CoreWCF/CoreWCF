1a2580f5cce591af38966fb14749fc5665d02fb9
g7ed6e <guillaume.delahaye@outlook.com>
2021-12-18 19:45:33 +0100

    Add tests to ensure HttpContext could be retrieved either from IServiceProvider or OperationContext.RequestContext.RequestMessage.Properties

---
+++ src/CoreWCF.Primitives/tests/Helpers/ServiceHelper.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Helpers;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace CoreWCF.Primitives.Tests.Helpers
+{
+    public static class ServiceHelper
+    {
+        public static IWebHostBuilder CreateWebHostBuilder<TStartup>(ITestOutputHelper outputHelper)
+            where TStartup : class
+        {
+            return WebHost.CreateDefaultBuilder(Array.Empty<string>())
+#if DEBUG
+            .ConfigureLogging((ILoggingBuilder logging) =>
+            {
+                logging.AddProvider(new XunitLoggerProvider(outputHelper));
+                logging.AddFilter("Default", LogLevel.Debug);
+                logging.AddFilter("Microsoft", LogLevel.Debug);
+                logging.SetMinimumLevel(LogLevel.Debug);
+            })
+#endif // DEBUG
+            .UseStartup<TStartup>();
+        }
+    }
+}
