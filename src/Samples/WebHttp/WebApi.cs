c5baae784a3018cbd6d1cfa1c338df839c00fd1e
Jonathan Hope <jonathan.hope@digimarc.com>
2022-04-11 12:06:35 -0700

    Further OpenAPI documentation generation improvements and a sample project.

---
+++ src/Samples/WebHttp/WebApi.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CoreWCF;
+
+namespace WebHttp
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    internal class WebApi : IWebApi
+    {
+        public string PathEcho(string param) => param;
+
+        public string QueryEcho(string param) => param;
+
+        public ExampleContract BodyEcho(ExampleContract param) => param;
+    }
+}
