6f9fb92a2c756af4500fd2579748170bedc0c2a8
Jon Louie <louiejon@amazon.com>
2022-04-18 15:57:25 -0700

    add MessageEncodingService to NetTcp.Tests

---
+++ src/CoreWCF.NetTcp/tests/ClientContract/IMessageEncodingService.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ClientContract
+{
+    [System.ServiceModel.ServiceContract]
+    public interface IMessageEncodingService
+    {
+        [System.ServiceModel.OperationContract]
+        byte[] EchoByteArray(byte[] bytes);
+    }
+}
