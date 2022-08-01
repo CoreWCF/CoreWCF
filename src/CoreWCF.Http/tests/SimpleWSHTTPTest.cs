2f552d7bccb38da61e62a3713c053053c4188528
Matt Connew <mconnew@users.noreply.github.com>
2022-08-01 13:00:43 -0700

    Reenable HttpSys tests using HTTPS

--- src/CoreWCF.Http/tests/SimpleWSHTTPTest.cs
+++ src/CoreWCF.Http/tests/SimpleWSHTTPTest.cs
@@ -230,8 +230,8 @@ namespace WSHttp
         }
 #endif
 
-        [Fact(Skip="Temporarily failing in Azure Devops due to corrupted IIS Express dev certificate in test environment image."),
-            Description("transport-security-with-windows-authentication-httpsys")]
+        [Fact]
+        [Description("transport-security-with-windows-authentication-httpsys")]
         [Trait("Category", "WindowsOnly")]  // HttpSys not supported on Linux
 #if NET5_0_OR_GREATER
         [System.Runtime.Versioning.SupportedOSPlatform("windows")]
@@ -284,8 +284,8 @@ namespace WSHttp
             }
         }
 
-        [Fact(Skip = "Failing in Azure Devops due to corrupted IIS Express dev certificate"),
-            Description("no-security-with-an-anonymous-client-using-impersonation-httpsys")]
+        [Fact]
+        [Description("no-security-with-an-anonymous-client-using-impersonation-httpsys")]
         [Trait("Category", "WindowsOnly")]  // HttpSys not supported on Linux
 #if NET5_0_OR_GREATER
         [System.Runtime.Versioning.SupportedOSPlatform("windows")]
