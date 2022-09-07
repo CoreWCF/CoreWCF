88097f929ccc3c1622483961b780a38e1608dc39
Matt Connew <mconnew@users.noreply.github.com>
2022-09-07 16:56:17 -0700

    Fix XsdDataContract not storing the XsdType

---
+++ src/CoreWCF.Metadata/tests/XmlSerializableTests.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ServiceContract;
+using Services;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using CoreWCF.Metadata.Tests.Helpers;
+
+namespace CoreWCF.Metadata.Tests
+{
+    public class XmlSerializableTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public XmlSerializableTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        [Trait("Category", "NetCoreOnly")] // DataSet outputs different Schema on NetFx (WSDL v1.0 ver WSDL v2.0)
+        public async Task SystemDataTest()
+        {
+            await TestHelper.RunSingleWsdlTestAsync<SystemDataService, ISystemDataService>(new BasicHttpBinding(), _output);
+        }
+    }
+}
