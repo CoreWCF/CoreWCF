48d429e1ce616fdc962d325eb3a85f15927573ba
lukedukeus <vanzweden1@gmail.com>
2022-10-18 13:34:09 -0700

    Update ConfigurationManagerExtensions.cs

--- src/CoreWCF.ConfigurationManager/src/CoreWCF/Configuration/ConfigurationManagerExtensions.cs
+++ src/CoreWCF.ConfigurationManager/src/CoreWCF/Configuration/ConfigurationManagerExtensions.cs
@@ -4,6 +4,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using System;
 
 namespace CoreWCF.Configuration
 {
