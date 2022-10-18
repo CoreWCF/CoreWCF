341461569458fced3e3201637bcf447990238c20
lukedukeus <vanzweden1@gmail.com>
2022-10-18 13:38:43 -0700

    Update using System.IO;

--- src/CoreWCF.ConfigurationManager/src/CoreWCF/Configuration/ConfigurationManagerExtensions.cs
+++ src/CoreWCF.ConfigurationManager/src/CoreWCF/Configuration/ConfigurationManagerExtensions.cs
@@ -4,7 +4,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
-using System;
+using System.IO;
 
 namespace CoreWCF.Configuration
 {
