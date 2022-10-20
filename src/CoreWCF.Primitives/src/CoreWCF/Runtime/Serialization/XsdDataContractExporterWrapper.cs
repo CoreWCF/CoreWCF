6aac7a1932da313fdf62960788a8bfc5dc7200b9
Guillaume Delahaye <681739+g7ed6e@users.noreply.github.com>
2022-10-20 09:25:22 +0200

    Ensure net7.0-rc2 support (#837)
    * Ensure .NET7 rc1 support
    
    * Increase test timeout to 15minutes
    
    * Move XsdDataContractExportFactory to correct namespace and location
    
    * Update test dependencies to .NET7 rc2
    
    * Inverse condition to pull Microsoft.AspNetCore.App in CoreWCF.NetTcp.Tests

---
+++ src/CoreWCF.Primitives/src/CoreWCF/Runtime/Serialization/XsdDataContractExporterWrapper.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace CoreWCF.Runtime.Serialization
+{
+    internal class XsdDataContractExporterWrapper : IXsdDataContractExporter
+    {
+        private readonly XsdDataContractExporter _xsdDataContractExporter;
+
+        public XsdDataContractExporterWrapper()
+        {
+            _xsdDataContractExporter = new XsdDataContractExporter();
+        }
+
+        public XsdDataContractExporterWrapper(XmlSchemaSet xmlSchemaSet)
+        {
+            _xsdDataContractExporter = new XsdDataContractExporter(xmlSchemaSet);
+        }
+
+        public void Export(Type type) => _xsdDataContractExporter.Export(type);
+
+        public XmlQualifiedName GetRootElementName(Type type) => _xsdDataContractExporter.GetRootElementName(type);
+
+        public XmlSchemaType GetSchemaType(Type type) => _xsdDataContractExporter.GetSchemaType(type);
+
+        public XmlQualifiedName GetSchemaTypeName(Type type) => _xsdDataContractExporter.GetSchemaTypeName(type);
+    }
+}
