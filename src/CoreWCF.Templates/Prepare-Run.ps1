57a70559815ec163f4a166f66a55049a29843c69
g7ed6e <guillaume.delahaye@outlook.com>
2022-10-21 09:00:26 +0200

    Use latest packages from CoreWCF private feed to run tests

--- src/CoreWCF.Templates/Prepare-Run.ps1
+++ src/CoreWCF.Templates/Prepare-Run.ps1
@@ -3,28 +3,26 @@ $SourceDir = [IO.Path]::GetFullPath([IO.Path]::Combine($PSScriptRoot, '..', '..'
 $TestTemplatesPath = [IO.Path]::Combine($SourceDir, 'TestTemplates')
 if (Test-Path $TestTemplatesPath) {
   Remove-Item -Path $TestTemplatesPath -Recurse -Force -ErrorAction Ignore
-} 
+}
 New-Item -Path $TestTemplatesPath -ItemType "directory" -Force
 $ArtifactsPath = [IO.Path]::Combine($TestTemplatesPath, 'Artifacts')
 New-Item -Path $ArtifactsPath -ItemType "directory" -Force
 $DirectoryBuildPropsContent = '<Project></Project>'
-$DirectoryBuildPropsContent | Out-File $TestTemplatesPath/Directory.Build.props 
+$DirectoryBuildPropsContent | Out-File $TestTemplatesPath/Directory.Build.props
 $DirectoryBuildTargetsContent = '<Project></Project>'
 $DirectoryBuildTargetsContent | Out-File $TestTemplatesPath/Directory.Build.target
 # Add a local nuget feed to publish current code packages
 $NugetFeedName = 'CoreWCFTemplates-Feed'
 dotnet nuget add source $ArtifactsPath -n $NugetFeedName
-# Pack current code (runtime assemblies) into the nuget feed
-dotnet pack $SourceDir/src/CoreWCF.Http/src/CoreWCF.Http.csproj -o $ArtifactsPath /p:IncludeSymbols=false
-dotnet pack $SourceDir/src/CoreWCF.Primitives/src/CoreWCF.Primitives.csproj -o $ArtifactsPath /p:IncludeSymbols=false
 # Store current code version in $version
 dotnet tool install --tool-path . nbgv
 $version = ./nbgv get-version -v NugetPackageVersion
 # install runtime assemblies in TemplatedProject
 $TemplatedProjectPath = [IO.Path]::Combine($PSScriptRoot, 'src', 'templates', 'CoreWCFService')
-cd $TemplatedProjectPath
-dotnet add package CoreWCF.Http -v $version
-cd $PSScriptRoot
+Set-Location $TemplatedProjectPath
+dotnet add package CoreWCF.Primitives -v 1.*-*
+dotnet add package CoreWCF.Http -v 1.*-*
+Set-Location $PSScriptRoot
 # Pack current code (templates) into the nuget feed
 dotnet build $SourceDir/src/CoreWCF.Templates/src/CoreWCF.Templates.csproj
 dotnet pack $SourceDir/src/CoreWCF.Templates/src/CoreWCF.Templates.csproj -o $ArtifactsPath /p:IncludeSymbols=false
