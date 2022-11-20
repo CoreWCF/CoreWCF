ac9edb94370eff27bdf0afe6579f7d0ea25f196b
Nicolas MANGUÉ <nicolas.mangue@gmail.com>
2022-11-20 19:41:53 +0100

    Make the facade compatible with the changes in net7.0

---
+++ src/CoreWCF.Primitives/src/CoreWCF/Security/NegotiateInternal/NTAuthenticationNet7.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace CoreWCF.Security.NegotiateInternal
+{
+    internal class NTAuthenticationNet7 : NTAuthenticationNet5
+    {
+        /*
+         * int Encrypt(
+         *     ReadOnlySpan<byte> buffer,
+         *     [NotNull] ref byte[]? output)
+         */
+        private static readonly Lazy<EncryptInvoker> s_encryptInvoker = new Lazy<EncryptInvoker>(() => BuildEncrypt());
+
+        public override int Encrypt(byte[] input, ref byte[] output) => s_encryptInvoker.Value(Instance, input, ref output);
+    }
+}
