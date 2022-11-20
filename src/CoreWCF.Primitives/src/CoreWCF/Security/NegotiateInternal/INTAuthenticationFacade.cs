c183978fe53fdd5d46f4d017945a689b4ed6b88d
Nicolas MANGUÉ <nicolas.mangue@gmail.com>
2022-11-20 17:49:30 +0100

    Move NTAuthentication calls to a specific class

---
+++ src/CoreWCF.Primitives/src/CoreWCF/Security/NegotiateInternal/INTAuthenticationFacade.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace CoreWCF.Security.NegotiateInternal
+{
+    internal interface INTAuthenticationFacade
+    {
+        object Instance { get; }
+
+        bool IsCompleted { get; }
+
+        string Protocol { get; }
+
+        bool IsValidContext { get; }
+
+        byte[] GetOutgoingBlob(byte[] incomingBlob, bool throwOnError, out object statusCode);
+
+        int Encrypt(byte[] input, ref byte[] output);
+
+        void CloseContext();
+    }
+}
