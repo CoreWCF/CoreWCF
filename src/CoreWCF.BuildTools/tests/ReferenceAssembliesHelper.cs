10fec898b20f9b4b9468298aab82a95e53217498
g7ed6e <guillaume.delahaye@outlook.com>
2023-01-03 21:47:46 +0100

    Use TypedConstant.ToCSharpString() to out put TypedConstant values in source generator

--- src/CoreWCF.BuildTools/tests/ReferenceAssembliesHelper.cs
+++ src/CoreWCF.BuildTools/tests/ReferenceAssembliesHelper.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
@@ -15,9 +16,9 @@ internal static class ReferenceAssembliesHelper
     internal static readonly Lazy<ReferenceAssemblies> Default = new(() =>
     {
         var coreWcfPrimitivesCsprojPath = Path.GetFullPath(Environment.CurrentDirectory + "../../../../../src/CoreWCF.Primitives/src/CoreWCF.Primitives.csproj");
-        var items = XDocument.Load(coreWcfPrimitivesCsprojPath);
-        var packages = items.Root!.Descendants("PackageReference").Select(x =>
-                new PackageIdentity(x.Attribute("Include")!.Value, x.Attribute("Version")!.Value))
+        var coreWcfWebHttpCsprojPath = Path.GetFullPath(Environment.CurrentDirectory + "../../../../../src/CoreWCF.WebHttp/src/CoreWCF.WebHttp.csproj");
+        var packages = ParsePackageReferences(coreWcfPrimitivesCsprojPath)
+            .Union(ParsePackageReferences(coreWcfWebHttpCsprojPath))
             .Union(new []
             {
                 new PackageIdentity("System.ServiceModel.Primitives", "4.10.0"),
@@ -26,4 +27,11 @@ internal static class ReferenceAssembliesHelper
             }).ToImmutableArray();
         return ReferenceAssemblies.Default.AddPackages(packages);
     });
+
+    private static IEnumerable<PackageIdentity> ParsePackageReferences(string csprojPath)
+    {
+        XDocument document = XDocument.Load(Path.GetFullPath(csprojPath));
+        return document.Root!.Descendants("PackageReference").Select(x =>
+            new PackageIdentity(x.Attribute("Include")!.Value, x.Attribute("Version")!.Value));
+    }
 }
