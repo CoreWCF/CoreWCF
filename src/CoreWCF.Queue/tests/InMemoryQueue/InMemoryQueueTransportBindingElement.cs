d7160de61e48e8dd13f2bc34c3ee1c6f757ea38f
g7ed6e <guillaume.delahaye@outlook.com>
2023-03-01 06:26:43 +0100

    Fix queue message neither acked nor nacked
    Add resiliency test
    
    Add disposable QueueTransportPump support

---
+++ src/CoreWCF.Queue/tests/InMemoryQueue/InMemoryQueueTransportBindingElement.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Concurrent;
+using CoreWCF.Channels;
+using CoreWCF.Configuration;
+using CoreWCF.Queue.Common;
+using CoreWCF.Queue.Common.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreWCF.Queue.Tests.InMemoryQueue;
+
+public class InMemoryQueueTransportBindingElement : QueueBaseTransportBindingElement
+{
+    public InMemoryQueueTransportBindingElement()
+    {
+
+    }
+
+    public InMemoryQueueTransportBindingElement(InMemoryQueueTransportBindingElement other)
+    {
+
+    }
+
+    public override BindingElement Clone() => new InMemoryQueueTransportBindingElement(this);
+
+    public override string Scheme => "inmem://";
+    public InMemoryMessageEncoding MessageEncoding { get; set; } = InMemoryMessageEncoding.Text;
+
+    public override QueueTransportPump BuildQueueTransportPump(BindingContext context)
+    {
+        var serviceProvider = context.BindingParameters.Find<IServiceProvider>();
+        var queue = serviceProvider.GetRequiredService<ConcurrentQueue<string>>();
+        var receiveContextInterceptor = serviceProvider.GetRequiredService<ReceiveContextInterceptor>();
+        return new InMemoryQueueTransportPump(queue, receiveContextInterceptor);
+    }
+}
