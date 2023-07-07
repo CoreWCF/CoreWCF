d0e9883e15fc605d071380a623c217d467020f9e
g7ed6e <guillaume.delahaye@outlook.com>
2023-07-07 20:48:24 +0200

    Set ServicePointManager.MaxServicePointIdleTime to 30s

--- src/CoreWCF.Http/tests/Program.cs
+++ src/CoreWCF.Http/tests/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace CoreWCF.Http.Tests
+﻿using System.Net;
+
+namespace CoreWCF.Http.Tests
 {
     /// <summary>
     /// Required because `<GenerateProgramFile />` is set to `false` to
     /// accommodate differences in behavior between `netcoreapp2.1` and
     /// `netcoreapp3.1`
-    /// </summary> 
-    internal static class Program { public static void Main(params string[] args) { } }
+    /// </summary>
+    internal static class Program
+    {
+        public static void Main(params string[] args)
+        {
+            ServicePointManager.MaxServicePointIdleTime = 1000 * 30;
+        }
+    }
 }
