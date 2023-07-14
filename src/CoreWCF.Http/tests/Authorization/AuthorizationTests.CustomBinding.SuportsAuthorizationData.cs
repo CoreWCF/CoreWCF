970511979ad3492a3c7f4c9c90dcd136dfb784a7
g7ed6e <guillaume.delahaye@outlook.com>
2023-07-14 17:01:00 +0200

    Add new public API to Ease AuthorizationData support with CustomBinding

---
+++ src/CoreWCF.Http/tests/Authorization/AuthorizationTests.CustomBinding.SuportsAuthorizationData.cs
@@ -0,0 +1,155 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel.Security;
+using CoreWCF.Channels;
+using CoreWCF.Configuration;
+using Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CoreWCF.Http.Tests.Authorization;
+
+public partial class AuthorizationTests
+{
+    [Fact]
+    public void CustomBindingSupportsAuthorizationData_AuthenticatedUser_HavingRequiredScopeValues_Test()
+    {
+        IWebHost host = ServiceHelper.CreateWebHostBuilder<CustomBindingSupportsAuthorizationDataWithAuthenticatedUserAndRequiredScopeValuesStartup>(_output).Build();
+        using (host)
+        {
+            host.Start();
+            System.ServiceModel.BasicHttpBinding binding = ClientHelper.GetBufferedModeBinding();
+            var factory = new System.ServiceModel.ChannelFactory<ISecuredService>(binding,
+                new System.ServiceModel.EndpointAddress(new Uri($"http://localhost:{host.GetHttpPort()}/BasicWcfService/custom.svc")));
+            ISecuredService channel = factory.CreateChannel();
+            string result = channel.Echo(TestString);
+            Assert.Equal(TestString, result);
+        }
+    }
+
+    [Fact]
+    public void CustomBindingSupportsAuthorizationData_UnauthenticatedUser_Test()
+    {
+        IWebHost host = ServiceHelper.CreateWebHostBuilder<CustomBindingSupportsAuthorizationDataWithUnauthenticatedUserStartup>(_output).Build();
+        using (host)
+        {
+            host.Start();
+            System.ServiceModel.BasicHttpBinding binding = ClientHelper.GetBufferedModeBinding();
+            var factory = new System.ServiceModel.ChannelFactory<ISecuredService>(binding,
+                new System.ServiceModel.EndpointAddress(new Uri($"http://localhost:{host.GetHttpPort()}/BasicWcfService/custom.svc")));
+            ISecuredService channel = factory.CreateChannel();
+            Assert.Throws<MessageSecurityException>(() => channel.Echo(TestString));
+        }
+    }
+
+    [Fact]
+    public void CustomBindingSupportsAuthorizationData_AuthenticatedUser_MissingScopeValues_Test()
+    {
+        IWebHost host = ServiceHelper.CreateWebHostBuilder<CustomBindingSupportsAuthorizationDataWithAuthenticatedUserButMissingScopeValuesStartup>(_output).Build();
+        using (host)
+        {
+            host.Start();
+            System.ServiceModel.BasicHttpBinding binding = ClientHelper.GetBufferedModeBinding();
+            var factory = new System.ServiceModel.ChannelFactory<ISecuredService>(binding,
+                new System.ServiceModel.EndpointAddress(new Uri($"http://localhost:{host.GetHttpPort()}/BasicWcfService/custom.svc")));
+            ISecuredService channel = factory.CreateChannel();
+            Assert.Throws<SecurityAccessDeniedException>(() => channel.Echo(TestString));
+        }
+    }
+
+    private abstract class CustomBindingSupportsAuthorizationDataStartup<TSecuredService> where TSecuredService : class, ISecuredService
+    {
+        public bool IsAuthenticated { get; set; }
+        public List<string> ScopeClaimValues { get; set; } = new();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<FakeJwtBearerAuthenticationHandlerOptions>(options =>
+            {
+                options.IsAuthenticated = IsAuthenticated;
+                options.ScopeClaimValues = ScopeClaimValues;
+            });
+
+            services.AddAuthentication("Basic")
+                .AddScheme<FakeJwtBearerAuthenticationHandlerOptions, FakeJwtBearerAuthenticationHandler>("Basic",
+                    options =>
+                    {
+                        options.IsAuthenticated = IsAuthenticated;
+                        options.ScopeClaimValues = ScopeClaimValues;
+                    });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Policies.Write,
+                    policy => policy.RequireAuthenticatedUser().RequireClaim("scope", DefinedScopeValues.Write));
+                options.AddPolicy(Policies.Read,
+                    policy => policy.RequireAuthenticatedUser().RequireClaim("scope", DefinedScopeValues.Read));
+            });
+            services.AddServiceModelServices();
+            services.AddHttpContextAccessor();
+            if (typeof(TSecuredService).IsInterface)
+            {
+                services.AddTransient<ISecuredService, TSecuredService>();
+            }
+            else
+            {
+                services.AddTransient<TSecuredService>();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseServiceModel(builder =>
+            {
+                builder.AddService<TSecuredService>();
+                BasicHttpBinding basicHttpBinding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly);
+                basicHttpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+                CustomBinding customBinding = new CustomBinding(basicHttpBinding);
+                customBinding.SupportsAuthorizationData = true;
+                builder.AddServiceEndpoint<TSecuredService, ISecuredService>(customBinding, "/BasicWcfService/custom.svc");
+            });
+        }
+    }
+
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    private class CustomBindingSupportsAuthorizationDataSecuredService : ISecuredService
+    {
+        [Authorize(Policy = Policies.Read)]
+        public string Echo(string text) => text;
+    }
+
+    private class CustomBindingSupportsAuthorizationDataWithAuthenticatedUserAndRequiredScopeValuesStartup : CustomBindingSupportsAuthorizationDataStartup<CustomBindingSecuredService>
+    {
+        public CustomBindingSupportsAuthorizationDataWithAuthenticatedUserAndRequiredScopeValuesStartup()
+        {
+            IsAuthenticated = true;
+            ScopeClaimValues.Add(DefinedScopeValues.Read);
+        }
+    }
+
+    private class CustomBindingSupportsAuthorizationDataWithUnauthenticatedUserStartup : CustomBindingSupportsAuthorizationDataStartup<CustomBindingSecuredService>
+    {
+        public CustomBindingSupportsAuthorizationDataWithUnauthenticatedUserStartup()
+        {
+            IsAuthenticated = false;
+            ScopeClaimValues.Clear();
+        }
+    }
+
+    private class CustomBindingSupportsAuthorizationDataWithAuthenticatedUserButMissingScopeValuesStartup : CustomBindingSupportsAuthorizationDataStartup<CustomBindingSecuredService>
+    {
+        public CustomBindingSupportsAuthorizationDataWithAuthenticatedUserButMissingScopeValuesStartup()
+        {
+            IsAuthenticated = true;
+            ScopeClaimValues.Clear();
+        }
+    }
+}
