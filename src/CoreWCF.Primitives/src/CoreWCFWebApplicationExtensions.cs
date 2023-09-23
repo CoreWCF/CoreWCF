c3d55bcf27a7ad1f8bc2c6bf703278dae9743f42
g7ed6e <guillaume.delahaye@outlook.com>
2023-09-23 10:27:00 +0200

    Fix ambiguous UseServiceModel
    temp
    
    aa

---
+++ src/CoreWCF.Primitives/src/CoreWCFWebApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace CoreWCF.Configuration
+{
+    public static class WebApplicationExtensions
+    {
+        public static Microsoft.Extensions.Hosting.IHost UseServiceModel(this Microsoft.AspNetCore.Builder.WebApplication app, Action<IServiceBuilder> configureServices)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            ((Microsoft.AspNetCore.Builder.IApplicationBuilder)app).UseServiceModel(configureServices);
+            return app;
+        }
+    }
+}
