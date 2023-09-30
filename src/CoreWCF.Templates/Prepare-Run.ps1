785d626df79c64ed47a0be913ad09afd02b693ec
Guillaume Delahaye <681739+g7ed6e@users.noreply.github.com>
2023-09-30 07:19:14 +0200

    Fix ambiguous UseServiceModel (#1235)

--- src/CoreWCF.Templates/Prepare-Run.ps1
+++ src/CoreWCF.Templates/Prepare-Run.ps1
@@ -19,14 +19,18 @@ dotnet nuget add source $ArtifactsPath -n $NugetFeedName
 # Store current code version in $version
 dotnet tool install --tool-path . nbgv
 $version = ./nbgv get-version -v NugetPackageVersion
+# package current runtime code in local nuget feed
+dotnet pack $SourceDir/src/CoreWCF.Primitives/src/CoreWCF.Primitives.csproj -o $ArtifactsPath /p:IncludeSymbols=false
+dotnet pack $SourceDir/src/CoreWCF.Http/src/CoreWCF.Http.csproj -o $ArtifactsPath /p:IncludeSymbols=false
 # install runtime assemblies in TemplatedProject
 $TemplatedProjectPath = [IO.Path]::Combine($PSScriptRoot, 'src', 'templates', 'CoreWCFService')
 Set-Location $TemplatedProjectPath
-dotnet add package CoreWCF.Primitives -v 1.*-*
-dotnet add package CoreWCF.Http -v 1.*-*
+dotnet nuget add source $ArtifactsPath -n $NugetFeedName
+dotnet add package CoreWCF.Primitives -v $version
+dotnet add package CoreWCF.Http -v $version
 Set-Location $PSScriptRoot
 # Pack current code (templates) into the nuget feed
 dotnet build $SourceDir/src/CoreWCF.Templates/src/CoreWCF.Templates.csproj
 dotnet pack $SourceDir/src/CoreWCF.Templates/src/CoreWCF.Templates.csproj -o $ArtifactsPath /p:IncludeSymbols=false
 # Install CoreWCF.Templates locally
-dotnet new --install CoreWCF.Templates::$version --nuget-source $ArtifactsPath
+dotnet new install CoreWCF.Templates::$version --nuget-source $ArtifactsPath
