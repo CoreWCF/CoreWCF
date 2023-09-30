027ae450d78261e91c8ca9f11311c0a6ba9881f7
Guillaume Delahaye <681739+g7ed6e@users.noreply.github.com>
2023-09-30 07:19:14 +0200

    Fix ambiguous UseServiceModel (#1235)

--- src/CoreWCF.Templates/Clean-Run.ps1
+++ src/CoreWCF.Templates/Clean-Run.ps1
@@ -5,6 +5,6 @@ if (Test-Path $TestTemplatesPath) {
   Remove-Item -Path $TestTemplatesPath -Recurse -Force -ErrorAction Ignore 
 }
 # Uninstall CoreWCF.Templates
-dotnet new --uninstall CoreWCF.Templates
+dotnet new uninstall CoreWCF.Templates
 # Delete the temporary feed added above
 dotnet nuget remove source CoreWCFTemplates-Feed
