d39285e7446b98e8361c197285108b6fc4dd8d8d
Matt Connew <mconnew@users.noreply.github.com>
2024-04-03 19:42:17 -0700

    Upgrade xunit to enable using testEnvironments.json to run on docker/WSL
    Adding docker compose to solution file

--- src/Common/tests/XunitTestAssemblyAttribute.NoConcurrencyNetFxOnly.cs
+++
@@ -1,8 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#if NETFRAMEWORK
-[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
-#else
-[assembly: Xunit.CollectionBehavior(MaxParallelThreads = -1)]
-#endif
