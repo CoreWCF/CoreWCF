f180ea8082c2a8beb22ef2cdc0bff3117ebd6406
g7ed6e <guillaume.delahaye@outlook.com>
2024-05-02 15:52:18 +0200

    clean usings

--- src/CoreWCF.BuildTools/src/OperationInvokerGenerator.Roslyn4.0.cs
+++ src/CoreWCF.BuildTools/src/OperationInvokerGenerator.Roslyn4.0.cs
@@ -1,16 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
-using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Text;
-using static CoreWCF.BuildTools.OperationParameterInjectionGenerator;
 
 namespace CoreWCF.BuildTools
 {
