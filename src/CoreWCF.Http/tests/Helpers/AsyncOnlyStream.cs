110a15313171d75fd49657ee0322fa8306007590
Marc Wils <marc.wils@vecozo.nl>
2024-11-24 20:34:43 +0100

    Use async IO to write MTOM XOP includes in responses

---
+++ src/CoreWCF.Http/tests/Helpers/AsyncOnlyStream.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreWCF.Http.Tests.Helpers
+{
+    internal class AsyncOnlyStream : Stream
+    {
+        private readonly Stream _inner;
+
+        public AsyncOnlyStream(Stream inner)
+        {
+            _inner = inner;
+        }
+
+        public override bool CanRead => _inner.CanRead;
+
+        public override bool CanSeek => _inner.CanSeek;
+
+        public override bool CanWrite => _inner.CanWrite;
+
+        public override long Length => _inner.Length;
+
+        public override long Position { get => _inner.Position; set => _inner.Position = value; }
+
+        public override void Flush() => throw new NotSupportedException("Synchronous IO is not allowed");
+        public override Task FlushAsync(CancellationToken cancellationToken) => _inner.FlushAsync(cancellationToken);
+
+        public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException("Synchronous IO is not allowed");
+
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => _inner.ReadAsync(buffer, offset, count, cancellationToken);
+
+        public override long Seek(long offset, SeekOrigin origin) => _inner.Seek(offset, origin);
+        public override void SetLength(long value) => _inner.SetLength(value);
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException("Synchronous IO is not allowed");
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => _inner.WriteAsync(buffer, offset, count, cancellationToken);
+    }
+}
--- src/CoreWCF.Primitives/src/CoreWCF/Channels/Message.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/Channels/Message.cs
@@ -748,7 +748,7 @@ namespace CoreWCF.Channels
             // otherwise EnsureWriteMessageState would get called twice. Also see OnWriteMessage()
             // for the example.
             await OnWriteBodyContentsAsync(writer);
-            WriteMessagePostamble(writer);
+            await WriteMessagePostambleAsync(writer);
         }
 
         private void EnsureWriteMessageState(XmlDictionaryWriter writer)
@@ -827,6 +827,15 @@ namespace CoreWCF.Channels
             {
                 writer.WriteEndElement();
                 writer.WriteEndElement();
+            }
+        }
+
+        internal async Task WriteMessagePostambleAsync(XmlDictionaryWriter writer)
+        {
+            if (Version.Envelope != EnvelopeVersion.None)
+            {
+                await writer.WriteEndElementAsync();
+                await writer.WriteEndElementAsync();
             }
         }
 
@@ -1173,7 +1182,7 @@ namespace CoreWCF.Channels
         {
             WriteMessagePreamble(writer);
             await OnWriteBodyContentsAsync(writer);
-            WriteMessagePostamble(writer);
+            await WriteMessagePostambleAsync(writer);
         }
 
         internal override Task OnWriteBodyContentsAsync(XmlDictionaryWriter writer)
