8db34b402561be18d8a3c591f73125b5c1bc09df
Marc Wils <marc.wils@vecozo.nl>
2024-12-19 12:58:23 +0100

    Use DelegatingStream in tests to reduce code in more specific helper stream implementations

--- src/CoreWCF.Http/tests/Helpers/AsyncOnlyStream.cs
+++ src/CoreWCF.Http/tests/Helpers/AsyncOnlyStream.cs
@@ -3,41 +3,20 @@
 
 using System;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace CoreWCF.Http.Tests.Helpers
 {
-    internal class AsyncOnlyStream : Stream
+    internal class AsyncOnlyStream : DelegatingStream
     {
-        private readonly Stream _inner;
-
         public AsyncOnlyStream(Stream inner)
+            : base(inner)
         {
-            _inner = inner;
         }
 
-        public override bool CanRead => _inner.CanRead;
-
-        public override bool CanSeek => _inner.CanSeek;
-
-        public override bool CanWrite => _inner.CanWrite;
-
-        public override long Length => _inner.Length;
-
-        public override long Position { get => _inner.Position; set => _inner.Position = value; }
-
         public override void Flush() => throw new NotSupportedException("Synchronous IO is not allowed");
-        public override Task FlushAsync(CancellationToken cancellationToken) => _inner.FlushAsync(cancellationToken);
 
         public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException("Synchronous IO is not allowed");
 
-        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => _inner.ReadAsync(buffer, offset, count, cancellationToken);
-
-        public override long Seek(long offset, SeekOrigin origin) => _inner.Seek(offset, origin);
-        public override void SetLength(long value) => _inner.SetLength(value);
         public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException("Synchronous IO is not allowed");
-
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => _inner.WriteAsync(buffer, offset, count, cancellationToken);
     }
 }
