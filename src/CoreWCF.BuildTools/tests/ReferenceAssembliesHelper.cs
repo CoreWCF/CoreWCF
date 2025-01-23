b4fab79b0261003089647d87e0b2cd5792422073
Matt Connew <mconnew@users.noreply.github.com>
2025-01-23 09:18:02 -0800

    Fixing up CPM dependent versions to resolve various issues

--- src/CoreWCF.BuildTools/tests/ReferenceAssembliesHelper.cs
+++ src/CoreWCF.BuildTools/tests/ReferenceAssembliesHelper.cs
@@ -7,12 +7,18 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.Testing;
 
 namespace CoreWCF.BuildTools.Tests;
 
 internal static class ReferenceAssembliesHelper
 {
+    static ReferenceAssembliesHelper()
+    {
+        MSBuildLocator.RegisterDefaults();
+    }
+
     internal static readonly Lazy<ReferenceAssemblies> Default = new(() =>
     {
         var coreWcfPrimitivesCsprojPath = Path.GetFullPath(Environment.CurrentDirectory + "../../../../../src/CoreWCF.Primitives/src/CoreWCF.Primitives.csproj");
@@ -30,8 +36,46 @@ internal static class ReferenceAssembliesHelper
 
     private static IEnumerable<PackageIdentity> ParsePackageReferences(string csprojPath)
     {
-        XDocument document = XDocument.Load(Path.GetFullPath(csprojPath));
-        return document.Root!.Descendants("PackageReference").Select(x =>
-            new PackageIdentity(x.Attribute("Include")!.Value, x.Attribute("Version")!.Value));
+        var projectCollection = new Microsoft.Build.Evaluation.ProjectCollection();
+        var project = projectCollection.LoadProject(csprojPath);
+        project.ReevaluateIfNecessary();
+
+        var packageVersionsList = project.GetItems("PackageVersion")
+            .Select(item => new PackageIdentity(item.EvaluatedInclude, item.GetMetadataValue("Version")))
+            .Where(identity => !string.IsNullOrEmpty(identity.Version));
+        Dictionary<string, PackageIdentity> packageVersions = new();
+        foreach (var packageVersion in packageVersionsList)
+        {
+            if (packageVersions.ContainsKey(packageVersion.Id))
+            {
+                packageVersions[packageVersion.Id] = packageVersion;
+            }
+            else
+            {
+                packageVersions.Add(packageVersion.Id, packageVersion);
+            }
+        }
+        var packageReferences = project.GetItems("PackageReference")
+            .Where(item => packageVersions.ContainsKey(item.EvaluatedInclude))
+            .Select(item =>
+            {
+                string version = string.Empty;
+                if (!string.IsNullOrEmpty(item.GetMetadataValue("Version")))
+                {
+                    version = item.GetMetadataValue("Version");
+                }
+                else if(!string.IsNullOrEmpty(item.GetMetadataValue("VersionOverride")))
+                {
+                    version = item.GetMetadataValue("VersionOverride");
+                }
+                else if(packageVersions.TryGetValue(item.EvaluatedInclude, out var packageIdentity))
+                {
+                    version = packageIdentity.Version;
+                }
+                return new PackageIdentity(item.EvaluatedInclude, version);
+            })
+            .Where(identity => !string.IsNullOrEmpty(identity.Version));
+
+        return packageReferences;
     }
 }
