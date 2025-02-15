70cf02ea4faf690edcdcb5ab321d3b2e16f85489
Stefan Nikolei <stefan@nikolei.eu>
2025-02-15 19:29:17 +0100

    Added an basic test to test that the Activity gets created

---
+++ src/CoreWCF.Primitives/tests/TelemetryTests.cs
@@ -0,0 +1,116 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWCF.Channels;
+using CoreWCF.Configuration;
+using CoreWCF.Telemetry;
+using Helpers;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace CoreWCF.Primitives.Tests;
+
+public class TelemetryTests
+{
+    [Fact]
+    public static async Task Basic_Telemetry_Test()
+    {
+        var startedActivites = new List<Activity>();
+        var stoppedActivites = new List<Activity>();
+
+        using var listener = new ActivityListener
+        {
+            ShouldListenTo = _ => true,
+            Sample = (ref ActivityCreationOptions<ActivityContext> _) => ActivitySamplingResult.AllData,
+            ActivityStarted = activity => startedActivites.Add(activity),
+            ActivityStopped = activity => stoppedActivites.Add(activity)
+        };
+
+
+        ActivitySource.AddActivityListener(listener);
+        string serviceAddress = "http://localhost/dummy";
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddServiceModelServices();
+        IServer server = new MockServer();
+        services.AddSingleton(server);
+        services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+        services.RegisterApplicationLifetime();
+        ServiceProvider serviceProvider = services.BuildServiceProvider();
+        IServiceBuilder serviceBuilder = serviceProvider.GetRequiredService<IServiceBuilder>();
+        serviceBuilder.BaseAddresses.Add(new Uri(serviceAddress));
+        serviceBuilder.AddService<SimpleService>();
+        var binding = new CustomBinding("BindingName", "BindingNS");
+        binding.Elements.Add(new MockTransportBindingElement());
+        serviceBuilder.AddServiceEndpoint<SimpleService, ISimpleService>(binding, serviceAddress);
+        await serviceBuilder.OpenAsync();
+        IDispatcherBuilder dispatcherBuilder = serviceProvider.GetRequiredService<IDispatcherBuilder>();
+        System.Collections.Generic.List<IServiceDispatcher> dispatchers =
+            dispatcherBuilder.BuildDispatchers(typeof(SimpleService));
+        Assert.Single(dispatchers);
+        IServiceDispatcher serviceDispatcher = dispatchers[0];
+        Assert.Equal("foo", serviceDispatcher.Binding.Scheme);
+        Assert.Equal(serviceAddress, serviceDispatcher.BaseAddress.ToString());
+        IChannel mockChannel = new MockReplyChannel(serviceProvider);
+        IServiceChannelDispatcher dispatcher =
+            serviceDispatcher.CreateServiceChannelDispatcherAsync(mockChannel).Result;
+        var requestContext = TestRequestContext.Create(serviceAddress);
+        dispatcher.DispatchAsync(requestContext).Wait();
+        Assert.True(requestContext.WaitForReply(TimeSpan.FromSeconds(5)), "Dispatcher didn't send reply");
+        requestContext.ValidateReply();
+
+        Assert.Single(startedActivites);
+        Assert.Single(stoppedActivites);
+
+        var startedActivity = startedActivites[0];
+        var stoppedActivity = stoppedActivites[0];
+
+        Assert.Equal("CoreWCF.Primitives.IncomingRequest", startedActivity.OperationName);
+        Assert.Equal("CoreWCF.Primitives.IncomingRequest", stoppedActivity.OperationName);
+        Assert.Equal("http://tempuri.org/ISimpleService/Echo", startedActivity.DisplayName);
+        Assert.Equal("http://tempuri.org/ISimpleService/Echo", stoppedActivity.DisplayName);
+        Assert.Equal(startedActivity.RootId, stoppedActivity.RootId);
+        Assert.Equal(startedActivity.SpanId, stoppedActivity.SpanId);
+
+        var startedTags = startedActivity.Tags.ToList();
+        var stoppedTags = stoppedActivity.Tags.ToList();
+        Assert.True(startedTags.Count == 7);
+        Assert.True(stoppedTags.Count == 7);
+
+        Assert.Equal("rpc.system", startedTags[0].Key);
+        Assert.Equal("dotnet_wcf", startedTags[0].Value);
+
+        Assert.Equal("rpc.method", startedTags[1].Key);
+        Assert.Equal("http://tempuri.org/ISimpleService/Echo", startedTags[1].Value);
+
+        Assert.Equal("soap.message_version", startedTags[2].Key);
+        Assert.Equal("Soap11 (http://schemas.xmlsoap.org/soap/envelope/) AddressingNone (http://schemas.microsoft.com/ws/2005/05/addressing/none)", startedTags[2].Value);
+
+        Assert.Equal("server.address", startedTags[3].Key);
+        Assert.Equal("localhost", startedTags[3].Value);
+
+        Assert.Equal("wcf.channel.scheme", startedTags[4].Key);
+        Assert.Equal("http", startedTags[4].Value);
+
+        Assert.Equal("wcf.channel.path", startedTags[5].Key);
+        Assert.Equal("/dummy", startedTags[5].Value);
+
+        Assert.Equal("soap.reply_action", startedTags[6].Key);
+        Assert.Equal("http://tempuri.org/ISimpleService/EchoResponse", startedTags[6].Value);
+
+        Assert.Equivalent(startedTags[1], stoppedTags[1]);
+        Assert.Equivalent(startedTags[2], stoppedTags[2]);
+        Assert.Equivalent(startedTags[3], stoppedTags[3]);
+        Assert.Equivalent(startedTags[4], stoppedTags[4]);
+        Assert.Equivalent(startedTags[5], stoppedTags[5]);
+        Assert.Equivalent(startedTags[6], stoppedTags[6]);
+    }
+}
