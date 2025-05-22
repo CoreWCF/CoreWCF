2b6a886705d54332d324ee6f58f89a37febfa79d
g7ed6e <guillaume.delahaye@outlook.com>
2025-05-22 11:48:00 +0200

    Restore missing implementations

--- src/CoreWCF.Primitives/src/CoreWCF/IdentityModel/Tokens/SamlSerializer.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/IdentityModel/Tokens/SamlSerializer.cs
@@ -4,7 +4,6 @@
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 using CoreWCF.IdentityModel.Selectors;
-using CoreWCF.IdentityModel.Tokens;
 using MSSAmlSerializer = Microsoft.IdentityModel.Tokens.Saml.SamlSerializer;
 using Microsoft.IdentityModel.Tokens.Saml;
 
@@ -32,13 +31,13 @@ namespace CoreWCF.IdentityModel.Tokens
             }
 
             SecurityKeyIdentifier securityKeyIdentifier = CoreWCF.Security.SecurityUtils.CreateSecurityKeyIdentifier(assertion.Signature.KeyInfo);
-            SecurityKey verificationKey = SamlSerializer.ResolveSecurityKey(securityKeyIdentifier, outOfBandTokenResolver);
+            SecurityKey verificationKey = SamlSerializer.ResolveSecurityKey(securityKeyIdentifier, outOfBandTokenResolver); ;
             if (verificationKey == null)
             {
                 throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new SecurityTokenException(SR.Format(SR.SAMLUnableToResolveSignatureKey, "issuer")));
             }
 
-            var samlSecurityToken = new SamlSecurityToken(assertion)
+            var samlSecurityToken = new IdentityModel.Tokens.SamlSecurityToken(assertion)
             {
                 SigningToken = SamlSerializer.ResolveSecurityToken(securityKeyIdentifier, outOfBandTokenResolver)
             };
@@ -49,12 +48,67 @@ namespace CoreWCF.IdentityModel.Tokens
 
             foreach (SamlStatement stmt in assertion.Statements)
             {
-                // ...existing code...
+                SamlSubjectStatement samlSubject = (SamlSubjectStatement)stmt;
+                SecurityKeyIdentifier keyIdentifier = null;
+                SecurityToken securityToken = null;
+                if (samlSubject.Subject.KeyInfo != null)
+                {
+                    keyIdentifier = CoreWCF.Security.SecurityUtils.CreateSecurityKeyIdentifier(samlSubject.Subject.KeyInfo);
+                    securityToken = SamlSerializer.ResolveSecurityToken(securityKeyIdentifier, outOfBandTokenResolver);
+                }
+                InternalSamlSubjectStatement internalSamlSubjectStatement = new InternalSamlSubjectStatement(samlSubject, keyIdentifier, securityToken);
+                samlSecurityToken.SamlStatements.Add(internalSamlSubjectStatement);
             }
-            // ...existing code...
+
+            //do a final validation on sign.
+            assertion.Signature.SignedInfo.Verify(Microsoft.IdentityModel.Tokens.CryptoProviderFactory.Default);
             return samlSecurityToken;
         }
 
-        // ...existing code...
+        internal static SecurityKey ResolveSecurityKey(SecurityKeyIdentifier ski, SecurityTokenResolver tokenResolver)
+        {
+            if (ski == null)
+                throw DiagnosticUtility.ExceptionUtility.ThrowHelperArgumentNull(nameof(ski));
+
+            if (tokenResolver != null)
+            {
+                for (int i = 0; i < ski.Count; ++i)
+                {
+                    if (tokenResolver.TryResolveSecurityKey(ski[i], out SecurityKey key))
+                        return key;
+                }
+            }
+
+            if (ski.CanCreateKey)
+                return ski.CreateKey();
+
+            return null;
+        }
+
+        internal static SecurityToken ResolveSecurityToken(SecurityKeyIdentifier ski, SecurityTokenResolver tokenResolver)
+        {
+            SecurityToken token = null;
+
+            if (tokenResolver != null)
+            {
+                tokenResolver.TryResolveToken(ski, out token);
+            }
+
+            if (token == null)
+            {
+                // Check if this is a RSA key.
+                if (ski.TryFind(out RsaKeyIdentifierClause rsaClause))
+                    token = new RsaSecurityToken(rsaClause.Rsa);
+            }
+
+            if (token == null)
+            {
+                // Check if this is a X509RawDataKeyIdentifier Clause.
+                if (ski.TryFind(out X509RawDataKeyIdentifierClause rawDataKeyIdentifierClause))
+                    token = new X509SecurityToken(new X509Certificate2(rawDataKeyIdentifierClause.GetX509RawData()));
+            }
+
+            return token;
+        }
     }
 }
