64f2c019d970c942adf4b0d2066d38c8e1326672
Guillaume Delahaye <681739+g7ed6e@users.noreply.github.com>
2025-05-22 09:32:38 +0000

    Translate message to EN-US

--- src/CoreWCF.Primitives/src/CoreWCF/Security/SamlSerializer.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/Security/SamlSerializer.cs
@@ -5,7 +5,7 @@ using CoreWCF.IdentityModel.Tokens;
 
 namespace CoreWCF.Security
 {
-    [System.Obsolete("SamlSerializer a été déplacé vers CoreWCF.IdentityModel.Tokens.SamlSerializer. Veuillez utiliser le nouveau namespace.")]
+    [System.Obsolete("SamlSerializer has been moved to CoreWCF.IdentityModel.Tokens.SamlSerializer. Please use the new namespace.")]
     public class SamlSerializer : CoreWCF.IdentityModel.Tokens.SamlSerializer
     {
         public SamlSerializer() : base() { }
