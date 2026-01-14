0aaf82757a65ac7319529433e2d08ea619eea4d2
Matt Connew <mconnew@users.noreply.github.com>
2026-01-14 15:02:34 -0800

    Fix IPv6 for Kafka

--- src/CoreWCF.Kafka/tests/Helpers/KafkaContainerFixture.cs
+++ src/CoreWCF.Kafka/tests/Helpers/KafkaContainerFixture.cs
@@ -2,8 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using Docker.DotNet.Models;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Networks;
@@ -105,25 +108,58 @@ public sealed class KafkaContainerFixture : IAsyncLifetime
         const int saslPlainTextPort = 9094;
         const int saslSslPort = 9095;
         const int mtlsPort = 9096;
-        
+
         _kafkaContainer = new ContainerBuilder()
             .WithImage("confluentinc/cp-kafka:7.9.1")
             .WithName($"broker-{Guid.NewGuid():N}")
             .WithNetwork(_network)
             .WithNetworkAliases("broker")
-            .WithPortBinding(plainTextPort, 9092)
-            .WithPortBinding(sslPort, 9093)
-            .WithPortBinding(saslPlainTextPort, 9094)
-            .WithPortBinding(saslSslPort, 9095)
-            .WithPortBinding(mtlsPort, 9096)
+            .WithPortBinding(plainTextPort, plainTextPort)
+            .WithPortBinding(sslPort, sslPort)
+            .WithPortBinding(saslPlainTextPort, saslPlainTextPort)
+            .WithPortBinding(saslSslPort, saslSslPort)
+            .WithPortBinding(mtlsPort, mtlsPort)
             .WithBindMount(secretsPath, "/etc/kafka/secrets")
+            .WithCreateParameterModifier(p =>
+            {
+                if (Socket.OSSupportsIPv6)
+                {
+                    // Enable IPv6 in the container
+                    p.HostConfig ??= new HostConfig();
+                    p.HostConfig.PortBindings ??= new Dictionary<string, IList<PortBinding>>();
+                    p.HostConfig.PortBindings[$"{plainTextPort}/tcp"] = new List<PortBinding>
+                    {
+                      new() { HostIP = "0.0.0.0", HostPort = plainTextPort.ToString() }, // IPv4
+                      new() { HostIP = "::",      HostPort = plainTextPort.ToString() }  // IPv6
+                    };
+                    p.HostConfig.PortBindings[$"{sslPort}/tcp"] = new List<PortBinding>
+                    {
+                      new() { HostIP = "0.0.0.0", HostPort = sslPort.ToString() }, // IPv4
+                      new() { HostIP = "::",      HostPort = sslPort.ToString() }  // IPv6
+                    };
+                    p.HostConfig.PortBindings[$"{saslPlainTextPort}/tcp"] = new List<PortBinding>
+                    {
+                      new() { HostIP = "0.0.0.0", HostPort = saslPlainTextPort.ToString() }, // IPv4
+                      new() { HostIP = "::",      HostPort = saslPlainTextPort.ToString() }  // IPv6
+                    };
+                    p.HostConfig.PortBindings[$"{saslSslPort}/tcp"] = new List<PortBinding>
+                    {
+                      new() { HostIP = "0.0.0.0", HostPort = saslSslPort.ToString() }, // IPv4
+                      new() { HostIP = "::",      HostPort = saslSslPort.ToString() }  // IPv6
+                    };
+                    p.HostConfig.PortBindings[$"{mtlsPort}/tcp"] = new List<PortBinding>
+                    {
+                      new() { HostIP = "0.0.0.0", HostPort = mtlsPort.ToString() }, // IPv4
+                      new() { HostIP = "::",      HostPort = mtlsPort.ToString() }  // IPv6
+                    };
+                }
+            })
             .WithEnvironment("KAFKA_BROKER_ID", "1")
             .WithEnvironment("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")
             .WithEnvironment("ZOOKEEPER_SASL_ENABLED", "false")
             // Configure listener security protocol map - match docker-compose exactly
             .WithEnvironment("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "INTERNAL:PLAINTEXT,HOSTPLAINTEXT:PLAINTEXT,HOSTSSL:SSL,HOSTSASLPLAINTEXT:SASL_PLAINTEXT,HOSTSASLSSL:SASL_SSL,HOSTMTLS:SSL")
             .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", $"INTERNAL://broker:29092,HOSTPLAINTEXT://localhost:{plainTextPort},HOSTSSL://localhost:{sslPort},HOSTSASLPLAINTEXT://localhost:{saslPlainTextPort},HOSTSASLSSL://localhost:{saslSslPort},HOSTMTLS://localhost:{mtlsPort}")
-            .WithEnvironment("KAFKA_LISTENERS", $"INTERNAL://0.0.0.0:29092,HOSTPLAINTEXT://0.0.0.0:9092,HOSTSSL://0.0.0.0:9093,HOSTSASLPLAINTEXT://0.0.0.0:9094,HOSTSASLSSL://0.0.0.0:9095,HOSTMTLS://0.0.0.0:9096")
             .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "INTERNAL")
             .WithEnvironment("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
             .WithEnvironment("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
@@ -143,8 +179,7 @@ public sealed class KafkaContainerFixture : IAsyncLifetime
             .WithEnvironment("KAFKA_LISTENER_NAME_HOSTSASLSSL_SSL_CLIENT_AUTH", "none")  // No client auth for SASL+SSL
             .WithEnvironment("KAFKA_LISTENER_NAME_HOSTSSL_SSL_CLIENT_AUTH", "none")  // No client auth for SSL only
             .WithEnvironment("KAFKA_LISTENER_NAME_HOSTMTLS_SSL_CLIENT_AUTH", "required")  // Require client auth for MTLS
-            // JAAS configuration for SASL - match docker-compose
-            .WithEnvironment("KAFKA_OPTS", "-Djava.security.auth.login.config=/etc/kafka/secrets/broker_jaas.conf")
+            .WithEnvironment("KAFKA_OPTS", "-Djava.security.auth.login.config=/etc/kafka/secrets/broker_jaas.conf")  // JAAS configuration for SASL - match docker-compose
             .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(9092))
             .Build();
 
